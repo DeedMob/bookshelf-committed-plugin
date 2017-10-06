9e82d908011bb1e47f9e2799c5b412554f4d1871
davidfurlong <dvfurlong@googlemail.com>
2017-10-06 14:40:04 +0200

    Initial commit, adds bookshelf committed plugin

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# bookshelf-commits-plugin
+# bookshelf-committed-plugin
+
 A plugin for Bookshelf ORM to fire the `committed` event on `saved` and transaction completed successfully.
+
+`saved`: `insert` or `update`
+
+## Use case
+
+The bookshelf `created`, `saved`, `updated` events actually fire before the transaction completes. So if you're using a transaction, and you try to load the model or related models in the `on` handler, the models wont be loaded.
+
+## Example and definition
+
+```
+initialize(){
+  this.on('committed', (model, attrs, options, previousAttributes) => {
+    // do something
+  });
+}
+```
+
+```
+model: Bookshelf.Model,
+attrs: Object, // plain object of attributes that will be updated
+options: Object, // options passed to save, including method which is automatically added if not originally in save options
+previousAttributes: Object, // plain object of attributes prior to this save, (not neccessarily the same as prior to the transaction)
+
+## FAQ
+
+- If I do a save on a model twice in the same transaction, will `committed` event be fired twice?
+Yes
+
+- Why did you include previousAttributes when bookshelf supports this via model.previousAttributes() and other bookshelf event handlers don't have this parameter?
+Bookshelf's `previousAttributes()` is unreliable because its conceivable that the value gets overwritten by another `save`, before we retrieve the value in the `on` handler of the other `save`. Additionally with the case of multiple `save`s on the same model in the same transaction, the first `previousAttributes` will be overwritten by the time the `committed` events get called. Additionally it makes more sense to me to call the event handler with this value as an additional argument rather than storing it in the model itself, as its only relevant immediately post `save`.
---
+++ index.js
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+
+// Committed Plugin
+// Emits the 'committed' event on `update` or `insert` when the save transaction completes for all models
+// Note model.previousAttributes doesnt work with this.
+module.exports = function(Bookshelf) {
+  const proto = Bookshelf.Model.prototype;
+  const Model = Bookshelf.Model.extend({
+    save: function(key, value, options) {
+      let attrs;
+      // Handle both `"key", value` and `{key: value}` -style arguments.
+      if (key == null || typeof key === 'object') {
+        attrs = key || {};
+        options = _.clone(value) || {};
+      } else {
+        (attrs = {})[key] = value;
+        options = options ? _.clone(options) : {};
+      }
+      options.method = this.saveMethod(options);
+      const previousAttributes = _.clone(this.attributes) || {};
+
+      const save = proto.save.call(this, key, value, options);
+      if(options.method === 'insert' || options.method === 'update'){
+        if (options.transacting) {
+          options.transacting.__subscribers.push(() => {
+            this.triggerThen('committed', this, attrs, options, previousAttributes);
+          });
+        } else {
+          save.tap(model => model.triggerThen('committed', model, attrs, options, previousAttributes));
+        }
+      }
+
+      return save;
+    },
+  });
+  Bookshelf.transaction = function(originalContainer) {
+    const modelsToCommit = [];
+    let t;
+    const container = transactor => {
+      transactor.__subscribers = [];
+      t = transactor;
+      return originalContainer(transactor);
+    };
+    const args = Array.from(arguments);
+    const d = Bookshelf.knex.transaction.apply(this, [container, ...args.slice(1)])
+
+    d.then(() => {
+      t.__subscribers.forEach(subscriber => subscriber());
+    });
+    return d;
+  };
+
+  Bookshelf.Model = Model;
+};
